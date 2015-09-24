626044d5aae61a593d35bcb656d69967bd2c88b2
DevinStoen <devin.stoen@yahoo.com>
2015-09-23 17:58:10 -0700

    Muzzle flashes burstcontrol work

--- Assets/Weapons/Guns/GunFire/ConcreteFireTypes/SingleShotFireControl.cs
+++ Assets/Weapons/Guns/GunFire/ConcreteFireTypes/SingleShotFireControl.cs
@@ -2,38 +2,21 @@
 using System.Collections;
 
 public class SingleShotFireControl : GunFireControl {
-
-
-
+	
 	public SingleShotFireControl(Gun myGun)
 	{
 		myCurrentGun = myGun;
 	}
 
-
-
 	public override void gunControl()
 	{
 		if (Input.GetMouseButtonDown (0)) {
-			
-			//PlayerInventory.getCurrentWeapon().GetComponent<Gun>().fireShot();
 			myCurrentGun.fireShot();
 		}
 
 	}
 
-
-	public override int fireNow()
-	{
-		return fire;
-	}
 	
-
-	public override void setFire(int set)
-	{
-		fire = set;
-
-	}
 
 
 }
